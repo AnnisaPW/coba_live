91ca89603b375675efe25164ebbd8309ec50a5a7
Annisa Putri <annisaputriwahyuni46@gmail.com>
2024-06-13 16:50:14 +0700

    chore: setup firebase

--- lib/app/_index.dart
+++ lib/app/_index.dart
@@ -1,3 +1,5 @@
+import 'package:coba_live/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
 
